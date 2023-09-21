79a0f56005d0ea438de15b11a0450e99b6906dc1
arkar <arkarphyo200156@gmail.com>
2023-09-21 22:34:30 +0630

    edit style.css

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,24 +1,34 @@
-import React ,{useRef} from 'react'
-import "./style.css"
+import React, { useRef } from "react";
+import "./style.css";
 
-interface Prop{
-  todo:string,
-  setTodo:React.Dispatch<React.SetStateAction<string>>
-  handleAdd:(e: React.FormEvent)=>void,
+interface Prop {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
-const InputField:React.FC<Prop> = ({todo,setTodo,handleAdd}) => {
-  const inputRef=useRef<HTMLInputElement>(null)
+const InputField: React.FC<Prop> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <>
-     <form className="input" onSubmit={(e)=>{
-      handleAdd(e)
-       inputRef.current?.blur();
-    }}>
-        <input ref={inputRef} value={todo} onChange={e => setTodo(e.target.value)} type="text" placeholder="Enter a task" className='input_box'/>
-        <button className='btn_box'>Go</button>
-      </form> 
+      <form
+        className="input"
+        onSubmit={(e) => {
+          handleAdd(e);
+          inputRef.current?.blur();
+        }}
+      >
+        <input
+          ref={inputRef}
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          placeholder="Enter a task"
+          className="input_box"
+        />
+        <button className="btn_box">Go</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
